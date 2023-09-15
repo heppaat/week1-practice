a0f94faccae9a0acef60dc97ba9fa678ae4ea254
heppaat <pali.herrer@gmail.com>
2023-09-15 20:56:36 +0200

    layout changes and small js

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("hello, world");
+console.log("I am Pali");
+//setInterval(()=>console.log("hello"),1000);
+
+let x = 10;
+let y = 5;
+//console.log(x+y);
+x + y;
+let z = "csésze"
+console.log(z);
+z = "bogre";
+console.log(z);
